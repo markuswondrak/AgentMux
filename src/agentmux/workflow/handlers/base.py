cfad33e5c205332b063c20721e3b59191aab23b4
Markus <markus@example.com>
2026-04-14 20:58:18 +0200

    fix: import PhaseResult from phase_result module

--- src/agentmux/workflow/handlers/base.py
+++ src/agentmux/workflow/handlers/base.py
@@ -12,6 +12,7 @@ from dataclasses import dataclass
 from typing import TYPE_CHECKING
 
 from agentmux.workflow.event_router import ToolSpec, WorkflowEvent
+from agentmux.workflow.phase_result import PhaseResult
 
 if TYPE_CHECKING:
     from agentmux.workflow.transitions import PipelineContext
